f86a69091a67edac7995bf2917e7eb88bc90caab
Jahid_Hassan17 <jahid.hassan@orbitax.com>
2022-11-18 17:22:46 +0600

    make it better

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,11 @@ import { config } from "./config/config";
 
 const app = express();
 
+
+// Define Router
+app.use('/', router);
+
+
 app.use(express.static('build'));
 app.get('*', (req: any, res: any) => {
 	res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
@@ -31,9 +36,6 @@ app.use(cors({
 // request parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Define Router
-app.use('/', router);
 
 // 404 not found handler
 app.use(notFoundHandler);
